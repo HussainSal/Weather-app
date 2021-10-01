0ef43a6d057ec1f04228ac902d27feea27970712
unknown <salmanmian54321@gmail.com>
2021-10-01 19:34:35 +0530

    'updated'

--- src/App.js
+++ src/App.js
@@ -7,16 +7,16 @@ function App() {
   // weather temperature state
   const [weatherTemp, setWeatherTemp] = useState("15.9");
   const [weather, setWeather] = useState("");
-  const [cloudy, setCloudy] = useState(false);
-  const [clear, setClear] = useState(false);
-  const [rainy, setRainy] = useState(false);
+  // const [cloudy, setCloudy] = useState(false);
+  // const [clear, setClear] = useState(false);
+  // const [rainy, setRainy] = useState(false);
 
   const getWeatherTemp = (data) => {
     setWeatherTemp(data.temp.day);
     setWeather(data.weather[0].main);
-    data.weather[0].main == "Clouds" && setCloudy(true);
-    data.weather[0].main == "Clear" && setClear(true);
-    data.weather[0].main == "Rain" && setRainy(true);
+    // data.weather[0].main == "Clouds" && setCloudy(true);
+    // data.weather[0].main == "Clear" && setClear(true);
+    // data.weather[0].main == "Rain" && setRainy(true);
   };
 
   // <div className= {`App outterBox ${cloudy ? 'cloudy' : clear ? 'clear' : rainy ? 'rainy' : 'clear'}`}></div>
